7cf5aaa863876fa985ddd302a8f488c46b1121ab
Shawal Arefin <shawalarefin132@gmail.com>
2025-08-30 16:00:02 -0400

    Create fccArrAvg.js
    avg = sum of total nums / total nums

---
+++ fccArrAvg.js
@@ -0,0 +1,9 @@
+function getAverage(scores) {
+  let sum = 0;
+  for(var nums of scores){
+    sum += nums;
+  } return sum / scores.length
+}
+
+console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
