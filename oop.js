447e556ba77f9a836a5940a0a17a9c361c21b849
ShawalarefiN <shawalarefin132@gmail.com>
2025-09-09 16:50:44 +0600

    updated 3

--- oop.js
+++ oop.js
@@ -41,17 +41,55 @@ const book2 = new Book('Book two', 'Jane Doe', '2016')
 console.log(book1.getSummary())*/
 
 //prototype
+/*
 function Book(title, author, year){
     this.title = title;
     this.author = author;
     this.year = year;
 }
-Book.prototype.getSummary= function (){return `${this.title} was written by ${this.author} in ${this.year}`}
+// getSummary
+Book.prototype.getSummary= function (){
+    return `${this.title} was written by ${this.author} in ${this.year}`
+}
+// get Age
 Book.prototype.getAge= function () {
     const years = new Date().getFullYear() - this.year;
     return `${this.title} is ${years} old`
 }
+// Revise / change year
+Book.prototype.revise = function(newYear){
+    this.year = newYear;
+    this.revise = true;
+}
+
 const book1 = new Book('Book one', 'John Doe', '2013')
 const book2 = new Book('Book two', 'Jane Doe', '2016') 
+console.log(book2)
+book2.revise('2022')
+console.log(book2)*/
 
-console.log(book2.getAge())
+// Inheritance 
+/*
+function Book(title, author, year){
+    this.title = title;
+    this.author = author;
+    this.year = year;
+}
+Book.prototype.getSummary= function (){
+    return `${this.title} was written by ${this.author} in ${this.year}`
+}
+// magazine constructor
+function Magazine(title, author, year, month){
+    Book.call(this, title, author, year);
+    this.month = month;
+}
+// Inherit prototype
+Magazine.prototype = Object.create(Book.prototype)
+// Inheritance Magazine Object
+const mag1 = new Magazine('mag one', 'john doe', '2016', 'june')
+
+console.log(mag1)
+// after Inherit prototype
+console.log(mag1.getSummary())*/
+
+// continue afer 26:32
